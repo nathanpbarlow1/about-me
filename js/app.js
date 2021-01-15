0e8d2a7e5c37cac3cb015573bd1732fcd827d0cb
Nathan <npbarlow1@gmail.com>
2021-01-14 18:07:30 -0800

    resolved last 2 variable names

--- js/app.js
+++ js/app.js
@@ -8,31 +8,31 @@ alert('Welcome to my site ' + userName);
 //examples: YES, yes, YEs, yeS, Y, y,
 
 var liveLocationCity = prompt('Do I live in Seattle?').toLowerCase();
-if (questionOne === 'no' || questionOne === 'n') {
+if (liveLocationCity === 'no' || liveLocationCity === 'n') {
   alert('That is actually not the case ' + userName );
   // console.log('you are correct. ');
 }
 
 var liveLocationState = prompt('Do I live in Washington State?').toLowerCase();
-if (questionOne === 'yes' || questionOne === 'y') {
+if (liveLocationState === 'yes' || liveLocationState === 'y') {
   alert('Yes, that is correct! ' + userName );
   // console.log('you are correct. ');
 }
 
 var snowboardingParticipant = prompt('What about snowboarding, do you think I do that?').toLowerCase();
-if (questionOne === 'yes' || questionOne === 'y') {
+if (snowboardingParticipant === 'yes' || snowboardingParticipant === 'y') {
   alert('Yes, that is correct! ' + userName );
   // console.log('you are correct. ');
 }
 
-var questionOne = prompt('How about skateboarding, do you think I do that?').toLowerCase();
-if (questionOne === 'yes' || questionOne === 'y') {
+var skateboardParticipant = prompt('How about skateboarding, do you think I do that?').toLowerCase();
+if (skateboardParticipant === 'yes' || skateboardParticipant === 'y') {
   alert('Yes! That\'s actually how I became interested in snowboarding');
   // console.log('you are correct. ');
 }
 
-var questionOne = prompt('You are probably noticing a trend by now, so do you think I surf?').toLowerCase();
-if (questionOne === 'no' || questionOne === 'n') {
+var surfParticipant = prompt('You are probably noticing a trend by now, so do you think I surf?').toLowerCase();
+if (surfParticipant === 'no' || surfParticipant === 'n') {
   alert('No. Unfortunately it\'s too cold in Washington');
   // console.log('you are correct. ');
 
