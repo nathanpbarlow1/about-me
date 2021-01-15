828c8aa5cfbaa7d20e2d99cfc0a52f89e493e1af
Nathan <npbarlow1@gmail.com>
2021-01-14 18:02:15 -0800

    resolved linter errors re: alert and variable name specificity

--- js/app.js
+++ js/app.js
@@ -1,40 +1,42 @@
 'use strict';
 
-var username = prompt('Hi! What is your name?');
-alert('Welcome to my site' + username);
-alert('Have a good time,' + userName +'!');
+var userName = prompt('Hi! What is your name?');
+alert('Welcome to my site ' + userName);
+
 
 //write 5 questions. they MUST accept yes or no OR y or n IN ANY CASE
 //examples: YES, yes, YEs, yeS, Y, y,
 
-var questionOne = prompt('Do I live in Seattle?').toLowerCase();
-alert('That is actually not the case ' + userName );
+var liveLocationCity = prompt('Do I live in Seattle?').toLowerCase();
 if (questionOne === 'no' || questionOne === 'n') {
+  alert('That is actually not the case ' + userName );
   // console.log('you are correct. ');
 }
 
-var questionOne = prompt('Do I live in Washington State?').toLowerCase();
-alert('Yes, that is correct! ' + userName );
+var liveLocationState = prompt('Do I live in Washington State?').toLowerCase();
 if (questionOne === 'yes' || questionOne === 'y') {
+  alert('Yes, that is correct! ' + userName );
   // console.log('you are correct. ');
 }
 
-var questionOne = prompt('What about snowboarding, do you think I do that?').toLowerCase();
-alert('Yes, that is correct! ' + userName );
+var snowboardingParticipant = prompt('What about snowboarding, do you think I do that?').toLowerCase();
 if (questionOne === 'yes' || questionOne === 'y') {
+  alert('Yes, that is correct! ' + userName );
   // console.log('you are correct. ');
 }
 
 var questionOne = prompt('How about skateboarding, do you think I do that?').toLowerCase();
-alert('Yes! That is actually how I became interested in snowboarding');
 if (questionOne === 'yes' || questionOne === 'y') {
+  alert('Yes! That\'s actually how I became interested in snowboarding');
   // console.log('you are correct. ');
 }
 
 var questionOne = prompt('You are probably noticing a trend by now, so do you think I surf?').toLowerCase();
-alert('No. Unfortunately it is too cold in Washington');
 if (questionOne === 'no' || questionOne === 'n') {
+  alert('No. Unfortunately it\'s too cold in Washington');
   // console.log('you are correct. ');
+
+  alert('Have a good time,' + userName +'!');
 }
 
 
